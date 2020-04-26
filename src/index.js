fff2a604e928678b4bc4e5193c7c553f1a33b8e9
Harry Turnbull <hturnbull@live.co.uk>
2020-04-26 11:41:21 +0100

    import Thermostat

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Thermostat from './Thermostat.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Thermostat />, document.getElementById('root'));
